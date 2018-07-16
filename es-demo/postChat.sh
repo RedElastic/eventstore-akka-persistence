66529432e7e941e41ca279eaea8e2b3c60facf7a
Dana Harrington <dana.harrington@redelastic.com>
2018-07-16 17:28:02 -0400

    - fixed postChat script using jq (along with uuidgen)

--- es-demo/postChat.sh
+++ es-demo/postChat.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-uuid=$(uuidgen)
-cat chatMatch.json | sed/UID/$uuid/ | curl -i -H "Content-Type:application/vnd.eventstore.events+json" "http://localhost:2113/streams/chat" -d @-
+jq -n --arg UUID $(uuidgen) '[ { "eventId": $UUID, "eventType": "match", "data" : { "user1": "user-A", "user2": "user-B" } } ]' | curl -i -H "Content-Type:application/vnd.eventstore.events+json" "http://localhost:2113/streams/chat" -d @-
